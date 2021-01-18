8311ec41be0f7fde3fb2122b304eccd42eb82e34
SadeCJohnson <tvibezengineering321@gmail.com>
2021-01-18 16:28:43 -0500

    This program includes 3 classes in 1 file.
    1. Superclass - Person
    2. Subclass - Student, that implements the Person Superclass
    3. Day12_Inheritance class that make method calls to functions found in the Super and Subclasses.
    
    Having multiple classes in one file is not advised. However, for the purposes of chronology of 30 day challenge commits, I'm including all 3 classes in a single file. A cleaner solution can be found in the Day12_Inheritance package.

---
+++ src/main/java/Day12_Inheritance.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+class Person {
+    int idNum;
+    String first;
+    String last;
+    public Person(String firstName, String lastName, int idNumber) {
+        this.first = firstName;
+        this.last = lastName;
+        this.idNum = idNumber;
+    }
+    // Print person data
+    public void printPerson(){
+        System.out.println(
+                "Name: " + last + ", " + first
+                        + 	"\nID: " + idNum);
+    }
+}
+
+class Student extends Person { //this is really Inheritance.Student extends Inheritance.Person
+
+
+    private int[] testScores;
+
+    /*
+     *   Class Constructor
+     *
+     *   @param firstName - A string denoting the Inheritance.Person's first name.
+     *   @param lastName - A string denoting the Inheritance.Person's last name.
+     *   @param id - An integer denoting the Inheritance.Person's ID number.
+     *   @param scores - An array of integers denoting the Inheritance.Person's test scores.
+     */
+    // Write your constructor here
+    Student(String firstName, String lastName, int idNumber,int[] scores){
+        super(firstName, lastName, idNumber);
+        this.testScores = scores;
+    }
+
+    /*
+     *   Method Name: calculate
+     *   @return A character denoting the grade.
+     */
+    // Write your method here
+    public char calculate(){
+        int sum = 0;
+        int average;
+        for (int testScore : testScores) {
+            sum += testScore;
+        }
+        average = sum/testScores.length;
+        if(average >= 90 && average <= 100){
+            return 'O';
+        } else if(average >= 80 && average < 90){
+            return 'E';
+        } else if (average >= 70 && average < 80){
+            return 'A';
+        } else if (average >= 50 && average < 70){
+            return 'P';
+        } else if (average >= 40 && average < 55){
+            return 'D';
+        } else {
+            return 'T';
+        }
+    }
+}
+
+
+public class Day12_Inheritance {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String firstName = scan.next();
+        String lastName = scan.next();
+        int id = scan.nextInt();
+        int numScores = scan.nextInt();
+        int[] testScores = new int[numScores];
+        for(int i = 0; i < numScores; i++){
+            testScores[i] = scan.nextInt();
+        }
+        scan.close();
+
+        Student s = new Student(firstName, lastName, id, testScores);
+        s.printPerson();
+        System.out.println("Grade: " + s.calculate() );
+    }
+}
