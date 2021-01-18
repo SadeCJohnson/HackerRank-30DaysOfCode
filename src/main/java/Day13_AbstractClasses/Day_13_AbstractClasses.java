754bf15a2e511abc5a0ad81048723c9363ba97dc
SadeCJohnson <tvibezengineering321@gmail.com>
2021-01-18 16:43:38 -0500

    Creates an Abstract class package and the corresponding decoupled  classes required for successful program execution.
    This is a cleaner solution than the proposed for Day_13_AbstractClasses.java, however the execution remains the same.

---
+++ src/main/java/Day13_AbstractClasses/Day_13_AbstractClasses.java
@@ -0,0 +1,17 @@
+package Day13_AbstractClasses;
+
+import java.util.Scanner;
+
+class Day13_AbstractClasses {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
