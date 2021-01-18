5761a7301b6537020446485db1264af71fc077e9
SadeCJohnson <tvibezengineering321@gmail.com>
2021-01-18 17:03:40 -0500

    This program does the following:
    1. Creates a Calculator class that has a power(n, p)  method that calculates n to the pth power and returns that value.
    
    2. Throws an Exception if any input provided is a negative value.

---
+++ src/main/java/Day_17_More_Exceptions.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+//Write your code here
+class Calculator {
+
+    int power(int n, int p) throws Exception {
+        int result = 1;
+        if (n < 0 || p < 0) {
+            throw new Exception("n and p should be non-negative");
+        } else {
+            for (int i = 0; i < p; i++) {
+                result *= n;
+            }
+        }
+        return result;
+    }
+}
+
+
+public class Day_17_More_Exceptions {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
+
+
+
