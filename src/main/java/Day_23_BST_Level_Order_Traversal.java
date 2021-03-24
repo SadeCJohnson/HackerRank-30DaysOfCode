a47dde708802d655423d6f1cd863b3a0d74404ea
SadeCJohnson <tvibezengineering321@gmail.com>
2021-03-23 23:21:17 -0400

    Day 23's solution implements the levelOrder function to print the level-order traversal of the binary search tree.

---
+++ src/main/java/Day_23_BST_Level_Order_Traversal.java
@@ -0,0 +1,65 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class Day_23_BST_Level_Order_Traversal {
+
+    static void levelOrder(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        //Write your code here
+        if (root != null) {
+            queue.add(root);
+        }
+
+        while (!queue.isEmpty()) {
+            root = queue.poll();
+            System.out.print(root.data + " ");
+
+            if (root.left != null) {
+                queue.add(root.left);
+            }
+            if (root.right != null) {
+                queue.add(root.right);
+            }
+        }
+
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        Node root = null;
+        while (T-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        levelOrder(root);
+    }
+}
